2ac0f7a8d3f3c11336c5c22ae53f1d8fee53f5f6
JonathanF2008 <jofra013@osloskolen.no>
2026-04-08 14:28:57 +0200

    .

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+
 const express = require("express")
 const fs = require("fs")
 
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(express.static("public"))
 
-// Filnavn
+
 const NOTES_FILE = "data.json"
 const TODO_FILE = "todo.json"
 
@@ -16,7 +16,7 @@ if (fs.existsSync(NOTES_FILE)) {
     notes = JSON.parse(fs.readFileSync(NOTES_FILE))
 }
 
-// GET alle notater
+
 app.get("/notes", (req, res) => {
     res.json(notes)
 })
@@ -38,40 +38,50 @@ if (fs.existsSync(TODO_FILE)) {
 }
 
 app.get("/todos", (req, res) => {
-    if (fs.existsSync(TODO_FILE)) {
+    try {
         const data = fs.readFileSync(TODO_FILE)
-        todos = JSON.parse(data)
+        const todos = JSON.parse(data)
+        res.json(todos)
+    } catch {
+        res.json([])
     }
-    res.json(todos)
 })
 
 
 app.post("/todos", (req, res) => {
+    console.log("BODY:", req.body) // 👈 viktig
+
+    let todos = []
+    if (fs.existsSync(TODO_FILE)) {
+        todos = JSON.parse(fs.readFileSync(TODO_FILE))
+    }
+
     todos.push({ task: req.body.task, done: false })
+
     fs.writeFileSync(TODO_FILE, JSON.stringify(todos, null, 2))
+
     res.json({ message: "Todo lagt til" })
 })
 
-
 app.put("/todos/:index", (req, res) => {
-    const i = parseInt(req.params.index)
+    let todos = []
 
-    // LES FRA FIL FØRST (VIKTIG)
     if (fs.existsSync(TODO_FILE)) {
-        const data = fs.readFileSync(TODO_FILE)
-        todos = JSON.parse(data)
+        todos = JSON.parse(fs.readFileSync(TODO_FILE))
     }
+
+    const i = parseInt(req.params.index)
 
     if (todos[i]) {
         todos[i].done = req.body.done
 
         fs.writeFileSync(TODO_FILE, JSON.stringify(todos, null, 2))
-        res.json({ message: "Todo oppdatert" })
+
+        res.json({ message: "OK" })
     } else {
-        res.status(404).json({ message: "Todo ikke funnet" })
+        res.status(404).json({ message: "Not found" })
     }
 })
-
 
 app.listen(3000, '0.0.0.0', () => {
     console.log("Server running on port 3000")
